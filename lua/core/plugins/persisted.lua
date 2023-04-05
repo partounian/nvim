2d089cf8248952ea73f6cc7b02b647bc9e983f70
Patrick Artounian <patrick.artounian@insomniac.com>
2023-04-05 10:08:57 -0700

    update plugins & remove lazy = false from persisted

--- lua/core/plugins/persisted.lua
+++ lua/core/plugins/persisted.lua
@@ -1,6 +1,5 @@
 local M = {
   "olimorris/persisted.nvim",
-  lazy = false,
   config = function()
     require("persisted").setup({
       use_git_branch = true, -- create session files based on the branch of the git enabled repository
