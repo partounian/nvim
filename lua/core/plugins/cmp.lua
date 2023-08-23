6aadb5ec9325a02abffddf407e396e67029452d9
Patrick Artounian <patrick.artounian@insomniac.com>
2023-08-23 11:45:04 -0700

    remove old commented copilot plugins

--- lua/core/plugins/cmp.lua
+++ lua/core/plugins/cmp.lua
@@ -9,8 +9,6 @@ local M = {
     "hrsh7th/cmp-calc",
     "lukas-reineke/cmp-rg",
     "hrsh7th/cmp-nvim-lsp-signature-help",
-    -- "github/copilot.vim",
-    -- "hrsh7th/cmp-copilot",
     "saadparwaiz1/cmp_luasnip",
   },
   config = function()
