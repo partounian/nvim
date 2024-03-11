a78511b3f065785f344cfddba58dd6f895fdd6ba
Patrick Artounian <patrick.artounian@insomniac.com>
2024-03-11 16:03:58 -0700

    remove foldingRange for remove nvim-ufo

--- lua/core/plugins/lsp/lsp.lua
+++ lua/core/plugins/lsp/lsp.lua
@@ -6,11 +6,6 @@ local lsp_settings = require("core.plugins.lsp.settings")
 local capabilities = vim.lsp.protocol.make_client_capabilities()
 -- enable autocompletion via nvim-cmp
 capabilities = require("cmp_nvim_lsp").default_capabilities(capabilities)
--- enable folding capabilities for nvim-ufo
-capabilities.textDocument.foldingRange = {
-  dynamicRegistration = false,
-  lineFoldingOnly = true,
-}
 
 require("utils.functions").on_attach(function(client, buffer)
   require("core.plugins.lsp.keys").on_attach(client, buffer)
