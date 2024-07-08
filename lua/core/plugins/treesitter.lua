137fe5f02a7ced1ef9c12e02cb96cac46c53fba7
Patrick Artounian <patrick.artounian@insomniac.com>
2024-07-08 10:47:16 -0700

    try new ts_context_commentstring config

--- lua/core/plugins/treesitter.lua
+++ lua/core/plugins/treesitter.lua
@@ -35,6 +35,11 @@ local M = {
     require("ts_context_commentstring").setup({
       enable_autocmd = false,
     })
+    local get_option = vim.filetype.get_option
+    vim.filetype.get_option = function(filetype, option)
+      return option == "commentstring" and require("ts_context_commentstring.internal").calculate_commentstring()
+        or get_option(filetype, option)
+    end
 
     require("nvim-ts-autotag").setup()
   end,
