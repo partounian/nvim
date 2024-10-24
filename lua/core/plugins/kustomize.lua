c3e690b1f35a43338efbfcba889993d9c36df26f
Patrick Artounian <partounian@gmail.com>
2024-10-24 12:16:34 -0700

    allow kustomize to be disabled

--- lua/core/plugins/kustomize.lua
+++ lua/core/plugins/kustomize.lua
@@ -1,6 +1,7 @@
 local conf = vim.g.config.plugins.kustomize
 return {
   "allaman/kustomize.nvim",
+  enabled = conf.enabled,
   dependencies = "nvim-lua/plenary.nvim",
   ft = "yaml",
   opts = conf.opts,
