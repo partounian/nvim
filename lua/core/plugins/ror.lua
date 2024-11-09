ca47e40eb924483eef3afd62b054c90ca37d789f
Patrick Artounian <partounian@gmail.com>
2024-11-08 16:18:59 -0800

    add Test current line to ror

--- lua/core/plugins/ror.lua
+++ lua/core/plugins/ror.lua
@@ -6,5 +6,6 @@ return {
     { "<leader>rr", ":lua require('ror.routes').list_routes()<CR>", desc = "List routes" },
     { "<leader>rs", ":lua require('ror.routes').sync_routes()<CR>", desc = "Sync routes" },
     { "<leader>rt", ":lua require('ror.test').run()<CR>", desc = "Test current file" },
+    { "<leader>rl", ":lua require('ror.test').run('Line')<CR>", desc = "Test current line" },
   },
 }
