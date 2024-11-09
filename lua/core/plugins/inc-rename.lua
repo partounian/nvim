723e13ef6b1e471460e5996f81117ba015c4f903
Patrick Artounian <partounian@gmail.com>
2024-11-08 16:20:24 -0800

    return inc-rename settings to default

--- lua/core/plugins/inc-rename.lua
+++ lua/core/plugins/inc-rename.lua
@@ -1,7 +1,9 @@
 return {
   "smjonas/inc-rename.nvim",
   lazy = true,
-  opts = {
-    input_buffer_type = "dressing",
-  },
+  config = true,
+  -- tried to fix inc-rename which broke just for me?
+  -- opts = {
+  --   input_buffer_type = "dressing",
+  -- },
 }
