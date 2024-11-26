5442e6f951e0ccaeb110be7568ec9a090bbe332c
Patrick Artounian <partounian@gmail.com>
2024-11-25 17:45:25 -0800

    remove my inc-rename comments

--- lua/core/plugins/inc-rename.lua
+++ lua/core/plugins/inc-rename.lua
@@ -2,8 +2,4 @@ return {
   "smjonas/inc-rename.nvim",
   lazy = true,
   config = true,
-  -- tried to fix inc-rename which broke just for me?
-  -- opts = {
-  --   input_buffer_type = "dressing",
-  -- },
 }
